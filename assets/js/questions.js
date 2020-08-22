0bce6b7916639ac4961c44a10f9f991a45a0a023
Logan <logandpage5@gmail.com>
2020-08-22 15:29:25 -0600

    final

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,53 +1,28 @@
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
+  {
+      title: "Commonly Used data types DO NOT include:",
+      choices: ["stings", "alerts", "booleans", "numbers"],
       answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+  },
+  {
+      title: "The condition in an if / else statment is enclosed within _____.",
+      choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
       answer: "parentheses"
-    },
-    {
-      title: "Repeats until a specific condition becomes false:",
-      choices: ["What is the DOM?", "Do While Loop", "For Loop Example", "What is a variable?"],
-      answer:  "For Loop Example"
-    },
-    {
-      title: "Removes the first element of an array and returns that element",
-      choices: ["Dynamic Typing", "shift()", "Function", "push()"],
-      answer: "shift()"
-    },
-    {
-      title: "Returns the first Element within the document that matches the specified selector, or group of selectors, or null, if no matches are found.",
-      choices: ["What is variable 'Hoisting'", "document.querySelectorAll()", "Multi Paradigm Language", "document.querySelector()"],
-      answer: "document.querySelector()"
-    },
-    {
-      title: "Can read and alter the elements on a webpage",
-      choices: ["addEventListener", "For Loop Example", "intervalid", ".innerHTML"],
-      answer: ".innerHTML"
-    },
-    {
-      title: "True or False",
-      choices: ["What kind of value does Boolean take", "What will an undeclared variable return?", "What are the Strings within Datatypes?", "What are the two ways to add JS to your site?"],
-      answer: "What kind of value does Boolean take"
-    }, 
-    {
-      title: "Integer: Whole Number (-1,450) Float: Decimal Point (0.1, 12.454)",
-      choices: ["What happens if you call the 'document.write' function after the document has fully loaded?", "What are the two types of Numbers within Datatypes?", "What are the two ways to add JS to your site?","What type of language is JavaScript?"],
-      answer: "What are the two types of Numbers within Datatypes?"
-    },
-    {
-      title: "Representation of HTML Page...",
-      choices: ["What is the DOM?", "What is DOM?", "What is a variable", "Math.random()"],
-      answer: "What is DOM?"
-    },
-    {
-      title: "Adds new elements to the beginning of an array, and returns the new length",
-      choices: ["unshift()", "Operators", ".innerHTML", "Do While Loop"],
-      answer: "unshift()"
-    },
-    ///etc.
-  ];
+  },
+  {
+      title: "What javascipt method can we use to select an html element?",
+      choices: ["document.queryselector()", "document.getElementChild", "document.getElementById", "Both 1 and 3"],
+      answer: "Both 1 and 3"
+  },
+  {
+      title: "What html tag is NOT included in the HEAD tag?",
+      choices: ["link", "meta", "title", "header"],
+      answer: "header"
+  },
+  {
+      title: "What attribute is used in html to decorate content?",
+      choices: ["css", "class", "src", "style"],
+      answer: "style"
+  }
+]
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,247 +1,180 @@
-var timeEl = document.getElementById("timer");
-var secondsLeft = 70;
-var timerInterval;
+// Questions
+var questions = [{
+  title: "Which of the following function of an array object adds one or more elements to the front of an array and returns the new length of the array?",
+  choices: ["unshift", "sort", "splice", "toString"],
+  answer: "unshift"
+},
+{
+  title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+  choices: ["last", "put", "push", "pop"],
+  answer: "push"
+},
+{
+  title: " Which built-in method returns the characters in a string beginning at the specified location?",
+  choices: ["substr", "getSubstring", "slice", "None of the above."],
+  answer: "substr"
+},
+{
+  title: "Which of the following function of an array object adds and/or removes elements from an array?",
+  choices: ["toSource", "sort", "unshift", "splice"],
+  answer: "splice"
+},
+{
+  title: "Which of the following function of String object combines the text of two strings and returns a new string?",
+  choices: ["add", "concat", " merge", "append"],
+  answer: "concat"
+},
+{
+  title: "What does HTML stand for?",
+  choices: ["Hyper Tag Markup Language", "Hyper Text Markup Language", "Hyper Text Mark Language", "Hyperlinking Text Marking Langauge"],
+  answer: "Hyper Text Markup Language"
+},
+{
+  title: "What symbol indicates a tag?",
+  choices: ["Angle Brackets e.g. <>", "Curved Brackets e.g. {}", "Commas e.g. ''", "Exclamation Mark e.g. !"],
+  answer: "Angle Brackets e.g. <>"
+},
+{
+  title: "Whic built-in method calls a function for each element in the array?",
+  choices: ["while", "loop", "forEach", "None of the above"],
+  answer: "forEach"
+},
+{
+  title: "Which of the following will write the message 'ALERT!' in an alert box?",
+  choices: ["alertBox('ALERT!')", "alert('ALERT!')", "msgAlert('ALERT')", "alert('ALERT!')"],
+  answer: "alert('ALERT!')"
+},
+{
+  title: "How do you find the minimum of x and y using JavaScript?",
+  choices: ["min(x,y);", "Math.min(x,y)", "Math.min(xy)", "min(xy);"],
+  answer: "Math.min(x,y)"
+},
 
-var qTitle = document.getElementById("Question");
-var c1El = document.getElementById("btnA1");
-var c2El = document.getElementById("btnA2");
-var c3El = document.getElementById("btnA3");
-var c4El = document.getElementById("btnA4");
-var sTakeQ = document.getElementById("takeQuiz");
-var sScore = document.getElementById("finalScore");
-var sMain = document.getElementById("startQuiz");
-var msgEl = document.getElementById("result");
-var msgDone = document.getElementById("msgQuizDone");
-var msgScoreEl = document.getElementById("msgScore");
+]
 
-var numCorrectAnswers = 0;
-var numTotalQuestions = 0;
-var idxQuestion = 0;  // first question starts at 0
-var blnCorrect = false;
-var finalscore = 0;
-var blnFinalQuestion = false;
 
-sTakeQ.addEventListener("click", function (event) {
-  var element = event.target;
-  if (element.matches("button")) {
-    //which button did they click
-    // console.log("button clicked: " + element.id);
-    // alert("You clicked the "+element.getAttribute("data-answered")+" answer");
+//scores and timers 
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
 
-    msgEl.textContent = element.getAttribute("data-answered");
-    msgEl.style.color = "red";
-    // console.log(msgEl);
-    if (element.getAttribute("data-answered") === "Correct") {
-      blnCorrect = true;
-      msgEl.style.color = "green";
-      numCorrectAnswers++;
-    } else {
-      // incorrect answer, penalize them 15 second
-      secondsLeft -= 15;
-      checkTimeRemaining();
+function start() {
 
-    }
-    // alert("loading next question idxQuestion is"+idxQuestion);
-    // load the next question
-    idxQuestion++;
-    loadQuestion();
+timeLeft = 100;
+document.getElementById("timeLeft").innerHTML = timeLeft;
+
+timer = setInterval(function() {
+  timeLeft--;
+  document.getElementById("timeLeft").innerHTML = timeLeft;
+  if (timeLeft <= 0) {
+      clearInterval(timer);
+      endGame(); 
   }
+}, 1000);
 
-});
+next();
+}
 
-function loadQuestion() {
-  var idxCorrect = -99;
-  if (questions[idxQuestion] === undefined) {
-    // We have reached the end of file on our questions array
-    blnFinalQuestion = true;
-    //disable the buttons so they cannot keep scoring points!
-    disableQuiz();
-    return;
-  }
-  var correctAnswer = questions[idxQuestion].answer;
-  numTotalQuestions++;
+//stop the timer to end the game 
+function endGame() {
+clearInterval(timer);
 
-  //we have a global questions array already in memory, and the current index
-  qTitle.textContent = questions[idxQuestion].title;
+var quizContent = `
+<h2>Game over!</h2>
+<h3>You got a ` + score +  ` /100!</h3>
+<h3>That means you got ` + score / 10 +  ` questions correct!</h3>
+<input type="text" id="name" placeholder="First name"> 
+<button onclick="setScore()">Set score!</button>`;
 
-  //add processing to shuffle the answer choices in case they took this quick before
-  questions[idxQuestion].choices.sort(function() {
-          return 0.5 - Math.random();
-        });
+document.getElementById("quizBody").innerHTML = quizContent;
+}
 
-
-  // find out which choice is the correct one
-  for (i = 0; i < questions[idxQuestion].choices.length; i++) {
-    if (questions[idxQuestion].choices[i] === correctAnswer) {
-      idxCorrect = i;
-    }
-  }
-
-  c1El.textContent = questions[idxQuestion].choices[0];
-  c2El.textContent = questions[idxQuestion].choices[1];
-  c3El.textContent = questions[idxQuestion].choices[2];
-  c4El.textContent = questions[idxQuestion].choices[3];
-
-  c1El.setAttribute("data-answered", "Incorrect");
-  c2El.setAttribute("data-answered", "Incorrect");
-  c3El.setAttribute("data-answered", "Incorrect");
-  c4El.setAttribute("data-answered", "Incorrect");
-
-  switch (idxCorrect) {
-    case 0:
-      c1El.setAttribute("data-answered", "Correct");
-      break;
-    case 1:
-      c2El.setAttribute("data-answered", "Correct");
-      break;
-    case 2:
-      c3El.setAttribute("data-answered", "Correct");
-      break;
-    case 3:
-      c4El.setAttribute("data-answered", "Correct");
-      break;
-
-  }
-
+//local storage
+function setScore() {
+localStorage.setItem("highscore", score);
+localStorage.setItem("highscoreName",  document.getElementById('name').value);
+getScore();
 }
 
 
-function setTime() {
-  timerInterval = setInterval(function () {
-    secondsLeft--;
+function getScore() {
+var quizContent = `
+<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h1>` + localStorage.getItem("highscore") + `</h1><br> 
 
-    if (!secondsLeft > 0) {
-      secondsLeft = 0;
-    }
-    // timeEl.textContent = "Time: "+ secondsLeft;
-    timeEl.textContent = "Time: " + secondsLeft.toString().padStart(2, '0');
-    checkTimeRemaining();
+<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
 
-  }, 1000);
+`;
+
+document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-function checkTimeRemaining() {
+//clear score
+function clearScore() {
+localStorage.setItem("highscore", "");
+localStorage.setItem("highscoreName",  "");
 
-  if (secondsLeft <= 0 || blnFinalQuestion) {
-    disableQuiz();
-    clearInterval(timerInterval);
-
-    showFinalScore();
-  }
+resetGame();
 }
 
-function showFinalScore() {
-  // hide the main section and the quiz section
-  sTakeQ.classList.add("d-none")
-  // sMain.classList.add("d-none");
-  sScore.classList.remove("d-none");
-  document.getElementById("msgQuizDone").textContent = "All done!";
-  if (!secondsLeft > 0) {
-    secondsLeft = 0;
-  }
+//reset the game 
+function resetGame() {
+clearInterval(timer);
+score = 0;
+currentQuestion = -1;
+timeLeft = 0;
+timer = null;
 
-  // var finalscore = 0;
-  // had scope issues, made global for now
-  finalscore = 0;
-  if (numCorrectAnswers > 0) {
+document.getElementById("timeLeft").innerHTML = timeLeft;
 
-    finalscore = Math.round(100 * (numCorrectAnswers / numTotalQuestions) + (0.2 * secondsLeft));
-    if (finalscore > 100) {
-      finalscore = 100;
-    }
-  }
-  console.log("note: Total questions= " + numTotalQuestions + "\n correct answers= " + numCorrectAnswers + "\n seconds left= " + secondsLeft + "\n final score= " + finalscore);
+var quizContent = `
+<h1>
+  JavaScript Quiz!
+</h1>
+<h3>
+  Click to play!   
+</h3>
+<button onclick="start()">Start!</button>`;
 
-  document.getElementById("msgScore").textContent = "Your final score is " + finalscore;
-
-  // document.getElementById("msgScore").textContent = "Your final score is "+ (idxCorrect/numTotalQuestions)+(0.2*secondsLeft);
-  // completely bogus waste of hours, the 2 lines above this work, and the 2 commented lines below do Not work
-  // msgDone.getElementById("msgQuizDone").textContent = "All done!";
-  // msgScoreEl.getElementById("msgScore").textContent = "Your final score is "+ (idxCorrect/numTotalQuestions)+(0.2*secondsLeft);
+document.getElementById("quizBody").innerHTML = quizContent;
 }
 
-function takeQuiz() {
-
-  //populate 1st quiz question
-  idxQuestion = 0;
-  loadQuestion();
-  // hide the main section
-  sMain.classList.add("d-none");
-  sTakeQ.classList.remove("d-none");
-
-  //  start timer 
-  setTime();
-
+//deduct 10 seconds
+function incorrect() {
+timeLeft -= 10; 
+next();
 }
 
-function disableQuiz() {
-  c1El.disabled = true;
-  c1El.classList.remove("btn-primary");
-  c1El.classList.add("btn-secondary");
-  c2El.disabled = true;
-  c2El.classList.remove("btn-primary");
-  c2El.classList.add("btn-secondary");
-  c3El.disabled = true;
-  c3El.classList.remove("btn-primary");
-  c3El.classList.add("btn-secondary");
-  c4El.disabled = true;
-  c4El.classList.remove("btn-primary");
-  c4El.classList.add("btn-secondary");
+//increases the score by 10 points
+function correct() {
+score += 10;
+next();
+}
+ 
+function next() {
+currentQuestion++;
+
+if (currentQuestion > questions.length - 1) {
+  endGame();
+  return;
 }
 
-document.querySelector("#startBtn").onclick = function (event) {
+var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
 
-  // user clicks the Start Quiz button
-  // hide the startQuiz section and show the takeQuiz section
-
-  if (event === null) {
-    return;
-  }
-
-  takeQuiz();
-}
-
-
-
-document.querySelector("#submitBtn").onclick = function (event) {
-
-  if (event === null) {
-    return;
-  }
-  //Don't allow user to click submit if they haven't entered initials
-  if (document.getElementById("xInitials").value.length === 0) {
-    alert("Please enter your initials to submit your quiz score.");
-    return;
-  }
-  if (finalscore === 0) {
-    alert("Sorry, your score must be above a zero to be saved on the wall of fame.");
-    return;
-  }
-  // store final score to localstorage
-  // first try to retrieve scores from local storage in case we've taken the quiz before
-
-  Scores = JSON.parse(localStorage.getItem('highscores'));
-
-  if (Scores !== null) {
-
-    Scores.push({
-      'initials': document.getElementById("xInitials").value,
-      'highscore': finalscore
-    });
+for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+  var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+  buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+  if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+      buttonCode = buttonCode.replace("[ANS]", "correct()");
   } else {
-    // Converting to JSON string with JSON.stringify()
-    // then saving with localStorage
-    Scores = [];
-    Scores.push({
-      'initials': document.getElementById("xInitials").value,
-      'highscore': finalscore
-    });
+      buttonCode = buttonCode.replace("[ANS]", "incorrect()");
   }
-  localStorage.setItem('highscores', JSON.stringify(Scores));
-  document.getElementById("submitBtn").disabled = true;
-  document.getElementById("submitBtn").remove("btn-primary");
-  // document.getElementById("submitBtn").add("btn-secondary");
-  sScore.classList.add("d-none");
-  document.location.href= "index.html";
-
+  quizContent += buttonCode
 }
+
+
+document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+
